f3d1197476180d3b791b6398908c6844030d8eda
unnoq <contact@unnoq.com>
2024-11-18 20:21:31 +0700

    openapi-playground

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { osw } from '../orpc'
+import { me, refresh, revoke, signin, signup } from './auth'
+import {
+  createPlanet,
+  deletePlanet,
+  findPlanet,
+  listPlanets,
+  updatePlanet,
+} from './planet'
+
+export const router = osw.router({
+  auth: osw.tags('Authentication').prefix('/auth').router({
+    signup,
+    signin,
+    refresh,
+    revoke,
+    me,
+  }),
+
+  planet: osw.tags('Planets').prefix('/planets').router({
+    list: listPlanets,
+    create: createPlanet,
+    find: findPlanet,
+    update: updatePlanet,
+    delete: deletePlanet,
+  }),
+})
